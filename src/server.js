dee73266d2840df3b44abc05c5dc5c27de6321d0
Felipe Bergamin <felipebergamin6@gmail.com>
2019-08-08 15:36:35 -0300

    code

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true });
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
